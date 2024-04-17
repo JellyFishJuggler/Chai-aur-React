e1a795f72a4d8d10f3a297d558fc954e631ccb29
Srijan Anand Gupta <guptasrijan85@gmail.com>
2024-04-17 20:48:31 +0530

    createBrowserRouter

--- react-router/src/main.jsx
+++ react-router/src/main.jsx
@@ -1,28 +1,50 @@
 import React, { Children } from 'react'
 import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Root from './Root'
 import Home from './component/Home'
 import About from './component/About'
+import Contact from './component/Contact'
 // import App from './App.jsx'
 import './index.css'
+import User from './component/User'
+import Github, { githubInfoLoader } from './component/Github/Github'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Root/>,
-    children: [
-      {
-        path: '',
-        element: <Home/>
-      },
-      {
-        path: 'about',
-        element: <About/>
-      }
-    ]
-  }
-])
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Root/>,
+      children: [
+        //Home
+        {
+          path: '',
+          element: <Home/>
+        },
+        //About
+        {
+          path: 'about',
+          element: <About/>
+        },
+        //Contact
+        {
+          path: 'contact',
+          element: <Contact/>
+        },
+        //User
+        {
+          path: 'user/:userid',
+          element: <User/>
+        },
+        //Github Followers
+        {
+          loader:githubInfoLoader,                      //yeh lag reduce krdeta h aur yeh ek api call krta h
+          path: 'github',
+          element: <Github/>
+        }
+      ]
+    }
+  ])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
